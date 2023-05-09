958e7e3dc2ceaa192ba7887d1b372cb79362af03
Javier Martí Valcárcel <undernightcore@gmail.com>
2023-05-09 22:29:01 +0200

    fix: unlimited text length by using the text type in db

--- database/migrations/1669464855581_projects.ts
+++ database/migrations/1669464855581_projects.ts
@@ -6,8 +6,8 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.string('name').notNullable().unique()
-      table.string('description').nullable()
+      table.text('name').notNullable().unique()
+      table.text('description').nullable()
       table
         .integer('forked_project_id')
         .unsigned()
