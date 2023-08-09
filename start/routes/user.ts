371e58a8ae204bac100dae5215c9b0791620bd7d
Javier Martí Valcárcel <undernightcore@gmail.com>
2023-08-09 11:47:58 +0200

    fix: edit user endpoint route

--- start/routes/user.ts
+++ start/routes/user.ts
@@ -5,6 +5,6 @@ Route.group(() => {
   Route.post('login', 'UserController.login')
   Route.post('register', 'UserController.register')
   Route.get(':id/verify', 'UserController.verify').as('verifyEmail')
-  Route.put('edit', 'UserController.edit')
+  Route.put('', 'UserController.edit')
   Route.put('email', 'UserController.editEmail')
 }).prefix('user')
