8c5bbf12ccabaef40bb56ab6558113c686fff62a
Javier Martí Valcárcel <undernightcore@gmail.com>
2023-08-25 15:48:13 +0200

    feat: created DisabledVerification.ts i18n

--- app/Middleware/DisabledVerification.ts
+++ app/Middleware/DisabledVerification.ts
@@ -2,9 +2,11 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Env from '@ioc:Adonis/Core/Env'
 
 export default class DisabledVerification {
-  public async handle({ response }: HttpContextContract, next: () => Promise<void>) {
+  public async handle({ response, i18n }: HttpContextContract, next: () => Promise<void>) {
     if (Env.get('DISABLE_VERIFICATION')) {
-      response.status(400).send({ errors: ['Email verification is disabled'] })
+      response
+        .status(400)
+        .send({ errors: [i18n.formatMessage('middleware.disabled_verification.disabled')] })
     } else {
       await next()
     }
