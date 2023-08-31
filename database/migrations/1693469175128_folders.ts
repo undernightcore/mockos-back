a7bb0b5d1abe7b33aec50f1f485e12183048bb9d
Javier Martí Valcárcel <undernightcore@gmail.com>
2023-08-31 11:23:50 +0200

    feat: create folders migration + model

---
+++ database/migrations/1693469175128_folders.ts
@@ -0,0 +1,38 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'routes'
+
+  public async up() {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.dropUnique(['project_id', 'endpoint', 'method'])
+
+      table.enum('method', ['get', 'post', 'put', 'delete', 'patch']).nullable().alter()
+      table.text('endpoint').nullable().alter()
+
+      table.boolean('is_folder').notNullable().defaultTo(false)
+      table
+        .integer('parent_folder_id')
+        .unsigned()
+        .nullable()
+        .references('routes.id')
+        .onDelete('SET NULL')
+    })
+  }
+
+  public async down() {
+    this.defer(async (db) => {
+      await db.from('routes').whereNull('endpoint').orWhereNull('method').delete()
+    })
+
+    this.schema.alterTable(this.tableName, (table) => {
+      table.enum('method', ['get', 'post', 'put', 'delete', 'patch']).notNullable().alter()
+      table.text('endpoint').notNullable().alter()
+
+      table.dropColumn('is_folder')
+      table.dropColumn('parent_folder_id')
+
+      table.unique(['project_id', 'endpoint', 'method'])
+    })
+  }
+}
